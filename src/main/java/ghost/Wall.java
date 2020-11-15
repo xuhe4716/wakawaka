c9f81994a2cd2299526fce739d72cf534e4848e6
xuhe4716 <62369147+xuhe4716@users.noreply.github.com>
2020-11-15 20:52:44 +0800

    Update Wall.java

--- src/main/java/ghost/Wall.java
+++ src/main/java/ghost/Wall.java
@@ -1,5 +1,5 @@
 package ghost;
-import processing.core.PApplet;
+//import processing.core.PApplet;
 import processing.core.PImage;
 
 
@@ -7,24 +7,52 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Wall{
+    /**
+     * row of the wall
+     */
     public int row;
+    /**
+     * Colum of the wall
+     */
     public int col;
+    /**
+     * Image of the wall
+     */
     public PImage image;
-    boolean nulll = false;
+    /**
+     * Marks if the wall is air or not
+     */
+    boolean air = false;
+    /**
+     * Character of the wall
+     */
     public String key;
+    /**
+     * pixel col of the wall
+     */
     public int pixel_x;
+    /**
+     * pixel row of the wall
+     */
     public int pixel_y;
-    String[] arr = new String[]{"a","c","i","w"};
-    List<String> ghost = Arrays.asList(arr);
 
 
+    /**
+     * Constructor for a wall, requires row, col image, and key
+     * default state of air bases on the image
+     * default state of pixel x and y base on col multiply 16 and row multiply 16
+     * @param row row of the wall
+     * @param col Colum of the wall
+     * @param image Image of the wall
+     * @param key Character of the wall
+     */
     public Wall(int row, int col, PImage image, String key){
         this.row = row;
         this.col = col;
         this.image = image;
         this.key = key;
         if(image == null){
-            this.nulll = true;
+            this.air = true;
         }
 
         this.pixel_x = this.row *16;
@@ -32,15 +60,23 @@ public class Wall{
     }
 
 
-
-
-    public void draw(PApplet app) {
-        if (!this.nulll && this.key!="p" && !this.ghost.contains(this.key) ) {
+    /**
+     * Draw the wall if it is not waka and ghost
+     * Given an app for image method
+     * @param app PApplet object for image method
+     */
+    public void draw(App app) {
+        String[] arr = new String[]{"a","c","i","w"};
+        List<String> ghost = Arrays.asList(arr);
+        if (!this.air && this.key!="p" && !ghost.contains(this.key) ) {
             app.image(this.image, row*16 , col*16);
         } else {
             return;
         }
     }
+
+
+
 
 
 }
