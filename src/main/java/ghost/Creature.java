f0897bb11a04a2d2f43d6a44a582041852203130
xuhe4716 <62369147+xuhe4716@users.noreply.github.com>
2020-11-16 18:26:21 +0800

    Update Creature.java

--- src/main/java/ghost/Creature.java
+++ src/main/java/ghost/Creature.java
@@ -1,39 +1,106 @@
 package ghost;
 
-import processing.core.PImage;
 
-import java.nio.channels.Pipe;
+import processing.core.PImage;
+import ghost.Waka;
+import ghost.App;
+import ghost.Ghost;
+
 import java.util.Arrays;
 import java.util.List;
 
-public class Creature {
-    int originalX;
-    int originalY;
-    PImage originalSprite;
-    int x;
-    int y;
-    int speed;
-    PImage sprite;
-    Wall[][] map;
+
+public abstract class Creature {
+    /**
+     * Initial value of row
+     */
+    public int originalX;
+    /**
+     * Initial value of column
+     */
+    public int originalY;
+    /**
+     * Initial image
+     */
+    public PImage originalSprite;
+    /**
+     * Row of creature
+     */
+    public int x;
+    /**
+     * Column of creature
+     */
+    public int y;
+    /**
+     * Speed of creature
+     */
+    public int speed;
+    /**
+     * Image of creature
+     */
+    public PImage sprite;
+    /**
+     * Map of game
+     */
+    public Wall[][] map;
+
+    /**
+     * Direction of creature.
+     * The second element stores current direction and the first element stores last direction.
+     */
+    public int[] direction = new int[2];
+    /**
+     * Pixel column of the creature
+     */
+    public int pixel_x;
+    /**
+     * Pixel row of the creature
+     */
+    public int pixel_y;
+    /**
+     * Upper bound of the map
+     */
+    public int upperBoundary;
+    /**
+     * Right bound of the map
+     */
+    public int rightBoundary;
+    /**
+     * Lower bound of the map
+     */
+    public int lowerBoundary;
+    /**
+     * Left bound of the map
+     */
+    public int leftBoundary;
 
 
-    int[] direction = new int[2];
-    int pixel_x;
-    int pixel_y;
 
-    int leftLowerBound;
-    int rightUpperBound;
-    int topUpeerBound;
-    int bottomLowerBound;
-    int targetX;
-    int targetY;
+    /**
+     * Current Direction of the creature
+     * From 37 to 40, which represent left, up, right, and down respectively
+     */
+    public int currentDirection;
+    /**
+     * Whether the creature can change direction to up or down
+     */
+    public boolean turnUpDown = false;
+    /**
+     * Whether the creature can change direction to left or right
+     */
+    public boolean turnLeftRight = false;
 
-
-    int currentDirection;
-    boolean turnUpDown = false;
-    boolean turnLeftRight = false;
-
-
+    /**
+     * Constructor for a creature, requires row, column, speed, image and map.
+     * Default value of pixel x and y base on row multiply 16 and column multiply 16.
+     * Default value of originalX and originalY base on row and column.
+     * Default value of originalSprite base on sprite.
+     * @param y Initial column
+     * @param x Initial row
+     * @param speed Speed read from config file
+     * @param sprite Initial image
+     * @param map Map of game
+     */
     public Creature(int y, int x, int speed, PImage sprite, Wall[][] map) {
         this.originalX = x;
         this.originalY = y;
@@ -48,13 +115,20 @@ public class Creature {
 
     }
 
-
+    /**
+     * Set value to direction.
+     * The second element stores current direction and the first element stores last direction.
+     * @param event From 37 to 40, which represent left, up, right, and down respectively
+     */
     public void setDirection(int event) {
         this.direction[0] = this.direction[1];
         this.direction[1] = event;
     }
 
-
+    /**
+     * The main method of controlling creature's movement.
+     * @param direction Direction of creature
+     */
     public void operation(int[] direction) {
         if (direction[1] == 39 && direction[0] == 0 || direction[1] == 39 && direction[0] == 37 || direction[1] == 39 && direction[0] == 39) { //initial start to right and left to right
             moveRight();
@@ -171,7 +245,9 @@ public class Creature {
 
     }
 
-
+    /**
+     * When the creature tries to turns right, set the row and column and make it move right.
+     */
     public void moveRight() {
         if (!collision(this.x, this.y + 1)) {
             this.pixel_y = (this.pixel_y + this.speed);
@@ -192,7 +268,9 @@ public class Creature {
 
     }
 
-
+    /**
+     * When the creature tries to turns left, set the row and column and make it move left.
+     */
     public void moveLeft() {
         if (!collision(this.x, this.y - 1)) {
             this.pixel_y = (this.pixel_y - this.speed);
@@ -211,7 +289,9 @@ public class Creature {
 
     }
 
-
+    /**
+     * When the creature tries to turns down, set the row and column and make it move down.
+     */
     public void moveDown() {
         if (!collision(this.x + 1, this.y)) {
             this.pixel_x = this.pixel_x + this.speed;
@@ -231,6 +311,9 @@ public class Creature {
 
     }
 
+    /**
+     * When the creature tries to turns up, set the row and column and make it move up.
+     */
     public void moveUp() {
         if (!collision(this.x - 1, this.y)) {
             this.pixel_x = (this.pixel_x - this.speed);
@@ -249,248 +332,92 @@ public class Creature {
 
     }
 
-
+    /**
+     * Check if the creature hits the wall at its current location.
+     * @param x Row of creature
+     * @param y Column of creature
+     * @return Whether the current location a wall
+     */
     public boolean collision(int x, int y) {
         String[] arr = new String[]{"a", "c", "i", "w"};
         List<String> ghost = Arrays.asList(arr);
-        if (map[x][y].key.equals("7") || map[x][y].nulll || map[x][y].key.equals("p") || ghost.contains(map[x][y].key) || map[x][y].key.equals("8")) {
+        if (map[x][y].key.equals("7") || map[x][y].air || map[x][y].key.equals("p") || ghost.contains(map[x][y].key) || map[x][y].key.equals("8")) {
             return false;
         }
         return true;
     }
 
-
-    public boolean OutOfBoundary(int x, int y, int direction) {
-
-        boolean outOfMap = false;
-        if (x < 0) {
-            x = 0;
-            outOfMap = true;
-        }
-        if(x >= 36) {
-            x = 35;
-            outOfMap = true;
-        }
-        if (y < 0) {
-            y = 0;
-            outOfMap = true;
-        }
-        if (y >= 28) {
-            y = 27;
-            outOfMap = true;
-        }
-
-
-
-        /*
-
-        if (x < 0 && y < 0) {
-            return OutOfBoundary(0, 0, direction);
-            //return true;
-        } else if (x > 36 && y > 28) {
-
-            /*
-            int xdiff = Math.abs(x - 36);
-            int ydiff = Math.abs(y - 28);
-
-             */
-            //return OutOfBoundary(35, 27, direction);
-            /*
-            if (xdiff < ydiff) {
-
-
-            } else {
-                OutOfBoundary(x, 27, 40);
-            }
-
-
-            //return true;
-
-             */
-        /*
-
-        }else if(x < 0 && y >28){
-
-            int xdiff = Math.abs(x);
-            int ydiff = Math.abs(y - 28);
-
-            if (xdiff < ydiff) {
-                OutOfBoundary(0, y, 37);
-            } else {
-                OutOfBoundary(x, 27, 40);
-            }
-
-
-
-
-            return OutOfBoundary(0, 27, direction);
-
-
-        }else if(x >36 && y <0) {
-
-            int xdiff = Math.abs(x - 36);
-            int ydiff = Math.abs(y);
-
-
-            if (xdiff < ydiff) {
-                OutOfBoundary(35, y, 39);
-            } else {
-                OutOfBoundary(x, 0, 38);
-            }
-
-
-
-
-            return OutOfBoundary(35, 0, direction);
-        }
-
-             */
-
-
-
-        //}else {
-        if (direction == 37) {
-            for (int i = 0; i < map[x].length; i++) {
-                if (Integer.parseInt(map[x][i].key) >= 1 && Integer.parseInt(map[x][i].key) <= 6) {
-                    leftLowerBound = i + 1;
-                    break;
-                }
-            }
-            if (y < leftLowerBound) {
-                //targetY = leftLowerBound;
-                return true;
-            } else {
-                return false || outOfMap;
-            }
-
-        } else if (direction == 39) {
-            for (int i = map[x].length - 1; i >= 0; i--) {
-                if (Integer.parseInt(map[x][i].key) >= 1 && Integer.parseInt(map[x][i].key) <= 6) {
-                    rightUpperBound = i - 1;
-                    break;
-                }
-            }
-            if (y > rightUpperBound) {
-                return true;
-                //targetY = rightUpperBound;
-            } else {
-                return false || outOfMap;
-            }
-        } else if (direction == 40) {
-            for (int i = map.length - 1; i >= 0; i--) {
-                if (Integer.parseInt(map[i][y].key) >= 1 && Integer.parseInt(map[i][y].key) <= 6) {
-                    bottomLowerBound = i - 1;
-                    break;
-                }
-            }
-            if (x > bottomLowerBound) {
-                //targetX = bottomLowerBound;
-                return true;
-            } else {
-                return false || outOfMap;
-            }
-
-
-        } else if (direction == 38) {
-            for (int i = 0; i < map.length; i++) {
-                if (Integer.parseInt(map[i][y].key) >= 1 && Integer.parseInt(map[i][y].key) <= 6) {
-                    topUpeerBound = i + 1;
-                    break;
-                }
-            }
-            if (x < topUpeerBound) {
-                //targetX = bottomLowerBound;
-                return true;
-            } else {
-                return false || outOfMap;
-            }
-        }
-        return false || outOfMap;
-    }
-
-
-
-
-
-        /*
-        if(x < 0 && y <0){
-            int xdiff = Math.abs(x);
-            int ydiff = Math.abs(y);
-
-            if(xdiff < ydiff){
-                return OutOfBoundary(0,y,37);
-            }else {
-                return OutOfBoundary(x,0, 38);
-            }
-        }else if(x > 36 && y > 28){
-            int xdiff = Math.abs(x-36);
-            int ydiff = Math.abs(y - 28);
-
-            if(xdiff < ydiff){
-                return OutOfBoundary(35, y,39);
-            }else{
-                return OutOfBoundary(x,27,40);
-            }
-        }else if(x < 0 && y>36)
-
-         */
-
-
-
-
-
-
-
-
-    /*
-    public boolean OutofMap(int x, int y) {
-        if (x < 0 && y < 0) {
-            int xdiff = Math.abs(x);
-            int ydiff = Math.abs(y);
-
-            if (xdiff < ydiff) {
-                return OutOfBoundary(0, y, 37);
-            } else {
-                return OutOfBoundary(x, 0, 38);
-            }
-        } else if (x > 36 && y > 28) {
-            int xdiff = Math.abs(x - 36);
-            int ydiff = Math.abs(y - 28);
-
-            if (xdiff < ydiff) {
-                return OutOfBoundary(35, y, 39);
-            } else {
-                return OutOfBoundary(x, 27, 40);
-            }
-
-        }else if(x < 0 && y >28){
-            int xdiff = Math.abs(x);
-            int ydiff = Math.abs(y - 28);
-
-            if (xdiff < ydiff) {
-                return OutOfBoundary(0, y, 37);
-            } else {
-                return OutOfBoundary(x, 27, 40);
-            }
-
-        }else if(x >36 && y <0){
-            int xdiff = Math.abs(x - 36);
-            int ydiff = Math.abs(y);
-
-
-            if (xdiff < ydiff) {
-                return OutOfBoundary(35, y, 39);
-            } else {
-                return OutOfBoundary(x, 0, 38);
-            }
-        }else {
-
-        }
-    }
-
+    /**
+     * Check whether the current position of the creature exceeds the boundary of the map.
+     * @param x Row of creature
+     * @param y Column of creature
+     * @return Whether the current position exceeds the boundary
      */
+    public boolean OutOfBoundary(int x, int y) {
+        setBoundary();
+        if (x < upperBoundary) {
+            return true;
+        }
+        if (x > lowerBoundary) {
+            return true;
+        }
+        if (y < leftBoundary) {
+            return true;
+        }
+        if (y > rightBoundary) {
+            return true;
+        }
+        return false;
+    }
 
 
+    /**
+     * If the current location of the creature exceeds the boundary of the map, find the closest point on the map to the current location.
+     * @param x Row of creature
+     * @param y Column of creature
+     * @return The coordinates of the closest point
+     */
+    public int[] nearestPoint(int x, int y){
+
+        setBoundary();
+        int[] nearestPoint = new int[2];
+        String[] arr = new String[]{"a","c","i","w"};
+        List<String> ghost = Arrays.asList(arr);
+
+
+        double distance = 999999;
+        int nearestX = 0;
+        int nearestY = 0;
+        for(int i = upperBoundary; i <= lowerBoundary+1; i++){
+            for(int j = leftBoundary; j <= rightBoundary+1; j++){
+                if (!ghost.contains(map[i][j].key)){
+                    double distance2 = Math.sqrt(Math.pow(x - i,2)+ Math.pow(y - j ,2));
+                    if(distance > distance2){
+                        distance = distance2;
+                        nearestX = i;
+                        nearestY = j;
+                    }
+
+                }
+            }
+
+        }
+        nearestPoint[0] = nearestX;
+        nearestPoint[1] = nearestY;
+        return nearestPoint;
+
+    }
+
+
+    /**
+     * Find the four corners of the map.
+     * @param map Map of game
+     * @return A two-dimensional array.
+     * The first element stores the coordinates of the upper left corner.
+     * The second element stores the coordinates of the upper right corner.
+     * The third element stores the coordinates of the lower left corner.
+     * The fourth element stores the coordinates of the lower right corner.
+     */
     public int[][] getConer(Wall[][] map){
         int[][] coner = new int[4][2];
         for(int i = 0 ; i<map.length;i++){
@@ -547,8 +474,21 @@ public class Creature {
         return coner;
     }
 
+    /**
+     * Set the boundary of map.
+     */
+    public void setBoundary(){
+        int[][] coner = getConer(map);
+        upperBoundary = Math.abs(coner[0][0]);
+        rightBoundary = 28 - coner[2][1] - coner[0][1];
+        lowerBoundary = 36 - coner[0][0];
+        leftBoundary = Math.abs(coner[0][1]);
+    }
 
 
+    /**
+     * Reset the creature.
+     */
     public void reset(){
         direction = new int[2];
         x = originalX;
@@ -561,8 +501,10 @@ public class Creature {
     }
 
 
-    public void draw(App app){
-        return;
-    }
+    /**
+     * @see Waka#draw(App)
+     * @see Ghost#draw(App)
+     */
+    public abstract void draw(App app);
 
 }
